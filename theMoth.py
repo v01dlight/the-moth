80f0c2b4078819642c59d4ce533aff27a243916b
Eamon <mulhollandeamon@gmail.com>
2021-08-24 12:49:17 -0500

    Update theMoth.py
    Made comments clearer

--- theMoth.py
+++ theMoth.py
@@ -21,10 +21,11 @@ async def on_message(message):
         await message.channel.send('Supported commands:\n?sooth - draws a random sooth card')
     
     if message.content.startswith('?sooth'):
-        # https://app.invisiblesunrpg.com/soothdeck/card-01/
-        # https://app.invisiblesunrpg.com/wpsite/wp-content/uploads/2018/04/02.png
+        # generate a random rumber between 1 and 60, with leading zeros if needed
         cardNum = str(random.randint(1,60)).zfill(2)
+        # post the link to the card image matching that number (Discord should auto-embed the image)
         await message.channel.send("https://app.invisiblesunrpg.com/wpsite/wp-content/uploads/2018/04/"+cardNum+".png")
+        # post the link to the details page for that card. The <> wrapping on the URL prevents Discord from embedding a link preview (looks cleaner that way)
         await message.channel.send("Details: <https://app.invisiblesunrpg.com/soothdeck/card-"+cardNum+"/>")
 
 client.run('YOUR BOT TOKEN HERE')
