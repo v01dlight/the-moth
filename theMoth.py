f5d6ea85327bfebabcf829b6a7093e9c21b9d24d
root <root@guest.guest>
2021-08-31 22:25:21 +0000

    auto commit by gitter function

--- theMoth.py
+++ theMoth.py
@@ -18,10 +18,10 @@ async def on_message(message):
         await message.channel.send('Hello!')
 
     if message.content.startswith('?help'):
-        await message.channel.send('Supported commands:\n?sooth - draws a random sooth card')
+        await message.channel.send('Supported commands:\n?sooth - draws a random sooth card\n?roll - rolls dice, eg ?roll 2d6 or ?roll d20 +5')
 
     if message.content.startswith('?sooth'):
-        # generate a random rumber between 1 and 60, with leading zeros if needed
+        # generate a random number between 1 and 60, with leading zeros if needed
         cardNum = str(random.randint(1,60)).zfill(2)
         # post the link to the card image matching that number (Discord should auto-embed the image)
         await message.channel.send("https://app.invisiblesunrpg.com/wpsite/wp-content/uploads/2018/04/"+cardNum+".png")
@@ -32,7 +32,7 @@ async def on_message(message):
         # split into the dice groups we're rolling
         args = message.content.split()[1:]
         if not args:
-            return await message.channel.send(room, 'Missing dice argument')
+            return await message.channel.send('Missing dice argument')
         try:
             res = []
             bonus = 0
@@ -44,7 +44,7 @@ async def on_message(message):
                 count, sides = arg.split('d')
                 sides = int(sides)
                 count = int(count or 1)
-                res += [1 + randrange(sides) for _ in range(count)]
+                res += [1 + random.randrange(sides) for _ in range(count)]
             if len(res) < 2 and not bonus:
                 # only rolled one die, just post its roll
                 return await message.channel.send(str(res[0]))
